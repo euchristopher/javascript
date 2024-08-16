4101f75b19ca5f01517e47a039e8877322306f9e
euchristopher <euchris.3030@gmail.com>
2024-08-16 12:05:58 -0300

    .

--- estudos/Aula014/ex004/script.js
+++ estudos/Aula014/ex004/script.js
@@ -1,15 +1,15 @@
 function tabuada() {
   const num = document.querySelector("input#txtn");
   const tab = document.getElementById("seltab");
-  tab.innerHTML = ""
-  if (num.value.length == 0){
+  tab.innerHTML = "";
+  if (num.value.length == 0) {
     alert("Por favor, digite um número!");
   } else {
-    const n = Number(num.value)
-    for (let i = 1; i <= 10; i++){
-      let item = document.createElement("option")
-      item.text = `${n} x ${i} = ${n*i}`
-      tab.appendChild(item)
+    const n = Number(num.value);
+    for (let i = 1; i <= 10; i++) {
+      let item = document.createElement("option");
+      item.text = `${n} x ${i} = ${n * i}`;
+      tab.appendChild(item);
     }
   }
 }
